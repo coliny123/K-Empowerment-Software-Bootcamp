b3c403ed2ef7977fb8ba2827c715cbeae9aa375f
dohwan <dh990921@naver.com>
2023-01-20 17:30:58 +0900

    10_08

--- day07.py
+++ day07.py
@@ -1,16 +1,20 @@
-#10_6
+#10_8
 class Element:
     def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+        self.hidden_name = name
+        self.hidden_symbol = symbol
+        self.hidden_number = number
 
-    def __str__(self):
-        return f"{self.name} {self.symbol} {self.number}"
+    def get_name(self):
+        return self.hidden_name
+    def get_symbol(self):
+        return self.hidden_symbol
+    def get_number(self):
+        return  self.hidden_number
 
 
 el_dict = {"name": "Hydrogen", "symbol": "H", "number": 1}
 
 hydrogen = Element(**el_dict)
 
-print(hydrogen)
+print(hydrogen.get_name(), hydrogen.hidden_symbol, hydrogen.hidden_number)

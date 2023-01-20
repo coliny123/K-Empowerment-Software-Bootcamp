7a194b13a8ad5b233b3335a4147c97feaac90c5c
dohwan <dh990921@naver.com>
2023-01-20 16:59:20 +0900

    10_05

--- day07.py
+++ day07.py
@@ -1,12 +1,16 @@
 #10_4
-
 class Element:
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
 
+    def info(self):
+        print(self.name, self.symbol, self.number)
 
-e = Element("Hydrogen", "H", 1)
 
-print(e.name, e.symbol, e.number)
+el_dict = {"name": "Hydrogen", "symbol": "H", "number": 1}
+
+e = Element(**el_dict)
+
+print(e.info())

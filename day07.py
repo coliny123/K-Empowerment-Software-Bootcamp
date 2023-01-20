b10055f001d5b9c5bc647a57c7f6db20372c256f
dohwan <dh990921@naver.com>
2023-01-20 17:22:29 +0900

    10_07

--- day07.py
+++ day07.py
@@ -5,12 +5,12 @@ class Element:
         self.symbol = symbol
         self.number = number
 
-    def dump(self):
-        print(self.name, self.symbol, self.number)
+    def __str__(self):
+        return f"{self.name} {self.symbol} {self.number}"
 
 
 el_dict = {"name": "Hydrogen", "symbol": "H", "number": 1}
 
-e = Element(**el_dict)
+hydrogen = Element(**el_dict)
 
-print(e.dump())
+print(hydrogen)

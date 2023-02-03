b9a9fcf3b328976396583aa78f46701af0045ab6
dohwan <dh990921@naver.com>
2023-02-03 18:42:14 +0900

    day01_homework

--- DSProject.py
+++ DSProject.py
@@ -21,6 +21,7 @@ memory = []
 head, current, pre = None, None, None
 dataArray = ["다현", "정연", "쯔위", "사나", "지효"]
 
+
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
 	node = Node()		# 첫 번째 노드
@@ -37,3 +38,7 @@ if __name__ == "__main__":
 
 	printNodes(head)
 	print(node.data)  # 마지막 노드인 지효가 node 라서 node.data는 마지막 노드의 데이터를 나타냄
+
+
+
+# 딕셔너리의 키값은 포켓몬 이름 벨류는 체력 2차원리스트로 안쪽리스트에 문자열

192ece5f7e0af0b87bb342351a47a95beefec475
dohwan <dh990921@naver.com>
2023-02-06 15:40:15 +0900

    day02 stack02

--- DSProject.py
+++ DSProject.py
@@ -1,31 +1,67 @@
-stack = [None for _ in range(5)]  # list comprehenthion
-# stack = [None, None, None, None, None]
+def is_stack_full():
+	global SIZE, stack,top
+	if top >= SIZE -1:
+		return True
+	return False
 
+
+def is_stack_empty():
+	global SIZE, stack,top
+	if top == -1:
+		return True
+	return False
+
+
+def push(data):
+	global SIZE, stack, top
+	if is_stack_full():
+		print("stack is FULL!!")
+		return
+	top = top + 1
+	stack[top] = data
+
+
+def pop():
+	global SIZE, stack, top
+	if is_stack_empty():
+		print("stack is EMPTY~")
+		return
+	temp = stack[top]
+	stack[top] = None
+	top -= 1
+	return temp
+
+
+def peek():
+	global SIZE, stack, top
+	if is_stack_empty():
+		print("stack is EMPTY~")
+		return None
+	return top
+
+
+SIZE = int(input("stack size : "))
+stack = [None for _ in range(SIZE)]
 top = -1
 
-top = top +1
-stack[top] = "커피"
-top = top +1
-stack[top] = "녹차"
-top = top +1
-stack[top] = "꿀물"
-top = top +1
-stack[top] = "오렌지주스"
-top = top +1
-stack[top] = "바닐라라떼"
+if __name__ == "__main__" :
+	while True:
+		select = input("삽입(i)/추출(e)/확인(v)/종료(x) 중 하나를 선택 ==> ").lower()
+		if select == 'x':
+			break
+		elif select == 'i':
+			data = input("input data ==> ")
+			push(data)
+			print("stack status : ", stack)
+		elif select == 'e':
+			data = pop()
+			print("extrated data ==> ", data)
+			print("stack status : ", stack)
+		elif select == 'v':
+			data = peek()
+			print("check data ==> ", data)
+			print("stack status : ", stack)
+		else :
+			print("input mismatch")
 
-print("----------Stack 내역 시작------------")
-for i in range(len(stack)-1, -1, -1):
-	print(stack[i])
-print("----------Stack 내역 끝------------")
-
-
-data = stack[top]
-stack[top] = None
-top = top -1
-print(data)
-
-print("----------Stack 내역 시작------------")
-for i in range(len(stack)-1, -1, -1):
-	print(stack[i])
-print("----------Stack 내역 끝------------")
+	print("program end!")

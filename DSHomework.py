0aaaf3b3dc9cdd1479160d34a3cea816cbac03ff
dohwan <dh990921@naver.com>
2023-02-06 18:14:14 +0900

    day02_homework_05_01

--- DSHomework.py
+++ DSHomework.py
@@ -1,63 +1,77 @@
-def find_data(name, power):
-    find_pos = -1
-    for i in range(len(pokemons)):
-        keys = list(pokemons[i].keys())
-        print(pokemons[i][keys[0]])
-        if power >= pokemons[i][keys[0]]:
-            find_pos = i
-            break
-
-    if find_pos == -1:
-        find_pos = len(pokemons)
-
-    new_pokemon_dict = {name: power}
-    insert_data(find_pos, new_pokemon_dict)
+import random
+from math import sqrt
 
 
-def insert_data(idx, new_pokemon_dict):
-    if idx < 0 or idx > len(pokemons):
-        print("범위를 벗어났습니다")
+class Node():
+    def __init__(self):
+        self.data = None
+        self.link = None
+
+
+def print_node(start):
+    current = start
+    if current == None:
         return
-    pokemons.append(None)
-    len_pokemons = len(pokemons)
-
-    for i in range(len_pokemons -1, idx, -1):
-        pokemons[i] = pokemons[i-1]
-        pokemons[i-1] = None
-
-    pokemons[idx] = new_pokemon_dict
-
-# def sort_data():
-#     keys = []
-#     for i in range(len(pokemons)):
-#         key = list(pokemons[i].keys())
-#         keys.append(key)
-#     po_at = []
-#     for key in keys:
-#         start = 0
-#         for i in range(start,len(pokemons)):
-#             po_at[i] = pokemons[i][key[0]]
-#             start += 1
-#             break
-#         for j in range(len(po_at)):
-#             if po_at[j] >= po_at[j+1]:
-#                 pokemons[j], pokemons[j+1] = pokemons[j+1], pokemons[j]
-#
-#     print(pokemons)
+    while current.link != head:
+        current = current.link
+        x, y = current.data[1:]
+        print(current.data[0], "편의점 거리", sqrt(x ** 2 + y ** 2))
+    print()
 
 
+def sort_stores(store):
+    global memory, head, current, pre
+
+    node = Node()
+    node.data = store
+    memory.append(node)
+
+    if head == None:
+        head = node
+        node.link = head
+        return
+
+    node_x, node_y = node.data[1:]
+    node_dist = sqrt(node_x ** 2 + node_y ** 2)
+    head_x, head_y = head.data[1:]
+    head_dist = sqrt(head_x ** 2 + head_y ** 2)
+
+    if head_dist > node_dist:
+        node.link = head
+        last = head
+        while last.link != head:
+            last = last.link
+        last.link = node
+        head = node
+        return
+
+    current = head
+    while current.link != head:
+        pre = current
+        current = current.link
+        curr_x, curr_y = current.data[1:]
+        curr_dist = sqrt(curr_x ** 2 + curr_y ** 2)
+        if curr_dist > node_dist:
+            pre.link = node
+            node.link = current
+            return
+
+    current.link = node
+    node.link = head
 
 
+head, current, pre = None, None, None
+memory = []
 
+if __name__ == "__main__" :
+    names = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    tuple_array = []
+    for name in names:
+        tuple_array.append((name, random.randint(1, 100), random.randint(1, 100)))
 
-if __name__ == "__main__":
-    pokemons = [{"피카츄":150}, {"라이츄":80}, {"파이리":50}, {"꼬북이": 15}]
+    print(tuple_array)
 
-    # sort_data()
-    name = input("추가할 포켓몬 : ")
-    power = int(input("공격력 : "))
+    for store in tuple_array:
+        sort_stores(store)
 
-    find_data(name, power)
-    print(pokemons)
-
-
+    print_node(head)

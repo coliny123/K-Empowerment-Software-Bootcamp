55fb3dcc252c1b9f65ba02791bc2a416e22f3d40
dohwan <dh990921@naver.com>
2023-02-07 17:06:39 +0900

    Day03_07_01

--- DSHomework.py
+++ DSHomework.py
@@ -1,67 +1,62 @@
-def is_stackempty():
-    global SIZE, song, top
-    if top == -1:
+def is_queue_full():
+    global SIZE, rear, front, queue
+    if rear == SIZE-1:
         return True
     else:
         return False
 
 
-def is_stackfull():
-    global SIZE, song, top
-    if top >= SIZE-1:
+def is_queue_empty():
+    global SIZE, rear, front, queue
+    if front == rear:
         return True
     else:
         return False
 
 
-def push(data):
-    global SIZE, song, top
-    if is_stackfull():
-        return
-    top += 1
-    song[top] = data
+def enqueue(data):
+    global SIZE, rear, front, queue
+    if is_queue_full():
+        print("queue is full")
+        return None
+    else:
+        rear += 1
+        queue[rear] = data
 
 
-def pop():
-    global SIZE, song, top
-    if is_stackempty():
-        return
-    data = song[top]
-    song[top] = None
-    top -= 1
-    return data
+def dequeue():
+    global SIZE, rear, front, queue
+    if is_queue_empty():
+        print("queue is empty")
+        return None
+    else:
+        front += 1
+        data = queue[front]
+        queue[front]= None
+
+        for i in range(front +1, SIZE):
+            queue[i-1] = queue[i]
+            queue[i] = None
+        front = -1
+        rear -= 1
+
+        return data
 
 
-SIZE = 100
-song = [None for _ in range(SIZE)]
-top = -1
+SIZE = 5
+rear = front = -1
+queue = [None for _ in range(SIZE)]
 
 if __name__ == "__main__":
-    with open("애국가.txt", "r", encoding="utf8")as rfp:
-        lyrics_array = rfp.readlines()
+    enqueue("민지")
+    enqueue("혜린")
+    enqueue("하니")
+    enqueue("혜인")
+    enqueue("다니엘")
 
-    print("------original------")
-    for line in lyrics_array:
-        push(line)
-        print(f"{line}", end=" ")
-    print()
-
-    print("------reverse------")
-    while True:
-        line = pop()
-        if line == None:
-            break
-
-        small_stack = [None for _ in range(len(line))]
-        small_top = -1
-        for ch in line:
-            small_top +=1
-            small_stack[small_top] = ch
-
-        while True:
-            if small_top == -1:
-                break
-            ch = small_stack[small_top]
-            small_top -=1
-            print(f"{ch}", end= "")
-
+    for i in queue:
+        if is_queue_empty():
+            print("식당 영업 종료!")
+        else:
+            print(f"대기줄 상태 : {queue}")
+            print(f"{dequeue()} 님 식당에 들어감")

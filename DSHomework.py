99d23b08114ffc058637fea746b7f0948c9afef1
dohwan <dh990921@naver.com>
2023-02-09 18:14:58 +0900

    Day05_11_02

--- DSHomework.py
+++ DSHomework.py
@@ -1,20 +1,23 @@
-def sorting_ary(ary):
-    for j in range(1, len(ary)):
-        for i in range(j, 0, -1):
-            if ary[i-1][1] > ary[i][1]:
-                ary[i-1], ary[i] = ary[i], ary[i-1]
+def sort_arr(arr):
+    for i in range(0, len(arr)-1):
+        min_idx = i
+        for k in range(i+1, len(arr)):
+            if arr[min_idx] > arr[k]:
+                min_idx = k
+        tmp = arr[i]
+        arr[i] = arr[min_idx]
+        arr[min_idx] = tmp
+    return arr
 
-    return ary
+two_dimension_arr = [[55, 33,250, 44], [88, 1, 76, 23], [199, 222, 38, 47], [155, 145, 20, 99]]
 
+one_demension_arr = []
 
+for i in range(len(two_dimension_arr)):
+    for k in range(len(two_dimension_arr[i])):
+        one_demension_arr.append(two_dimension_arr[i][k])
 
-
-score_ary = [["민지", 90], ["혜린", 85],["혜인", 50], ["하니", 65], ["다니엘", 42], ["카리나", 95]]
-
-print(f"정렬 전 : {score_ary}")
-score_ary = sorting_ary(score_ary)
-print(f"정렬 후 : {score_ary}")
-print("##조편성")
-
-for i in range(len(score_ary) // 2):
-    print(f"{score_ary[i][0]} : {score_ary[len(score_ary)-1-i][0]}")
+print(f"1차원 배열 정렬 전 : {one_demension_arr}")
+one_demension_arr = sort_arr(one_demension_arr)
+print(f"1차원 배열 정렬 후 : {one_demension_arr}")
+print(f"중앙값 : {one_demension_arr[len(one_demension_arr) // 2]}")

0f4e23c91457b8d4e0cec3bff3f3acbe14afb466
dohwan <dh990921@naver.com>
2023-02-13 16:52:10 +0900

    EchoSever & EchoClient

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,23 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.UnknownHostException;
+import java.net.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            URL url = new URL("http://www.google.com");
-            URLConnection urlConnection = url.openConnection();  // 연결 객체 생성
-            BufferedReader br = new BufferedReader(new
-                    InputStreamReader(urlConnection.getInputStream()));  // 버퍼 객체에 담겨 한줄씩 가져옴
-            String line;
-            while ((line = br.readLine()) != null) {  // 여기서 한줄 한줄씩 꺼냄
-                System.out.println(line);
-            }
-            br.close();
+        try (ServerSocket serverSocket = new  // try(~~~~){} => 파이썬의 try-with-catch 구문처럼 실행되고 끝난다음 자동으로 메모리에서 사라짐
+                ServerSocket(6000)){     // 원래 객체 사라질 때는 close() 를 선언해야 함
+            Object connection;
+            System.out.println("Waiting for connection.....");
+            Socket clientSocket =
+                    serverSocket.accept();
+            System.out.println("Connected to client");
         } catch (IOException ex) {
             // Handle exceptions
         }
---
+++ untitled/src/simple/echo/SimpleEchoServer.java
@@ -0,0 +1,35 @@
+package simple.echo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+public class SimpleEchoServer {
+    public static void main(String[] args) {
+        // BufferedReader br = null;  // try -with 안쓰면 이렇게 하고 finally에서 close()해야함
+        // PrintWriter pit = null;
+        try (ServerSocket serverSocket = new ServerSocket(6000)) {
+            Object connection;
+            System.out.println("연결을 기다리는 중 ..........");
+            Socket clientSocket =
+                    serverSocket.accept();
+            System.out.println("클라이언트가 연결되었습니다. ");
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println("클라이언트로 부터 받은 메세지: " + line);
+                    out.println(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } //finally {
+            //  br.close();
+            //}
+        } catch (IOException ex) {
+            // Handle exceptions
+        }
+    }
+}

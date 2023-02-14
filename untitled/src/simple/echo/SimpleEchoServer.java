9d72355665ffbfa0dab99b307a80fc46bc84cf78
dohwan <dh990921@naver.com>
2023-02-14 10:56:06 +0900

    simpleechoserver v02

--- untitled/src/simple/echo/SimpleEchoServer.java
+++ untitled/src/simple/echo/SimpleEchoServer.java
@@ -6,30 +6,51 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class SimpleEchoServer {
     public static void main(String[] args) {
-        // BufferedReader br = null;  // try -with 안쓰면 이렇게 하고 finally에서 close()해야함
-        // PrintWriter pit = null;
-        try (ServerSocket serverSocket = new ServerSocket(6000)) {
-            Object connection;
-            System.out.println("연결을 기다리는 중 ..........");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("클라이언트가 연결되었습니다. ");
+        System.out.println("에코 서버 시작됨");
+        try (ServerSocket serverSocket = new ServerSocket(9900)) {
+            System.out.println("클라이언트 접속 대기 중.....");
+            Socket clientSocket = serverSocket.accept();  // 접속 대기
+            System.out.println("클라이언트 접속됨.");
 
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  // client 소켓에서 getinputstream으로 가져옴
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {  // autoflush: 버퍼 자동으로 비워주는 것
-                String line;
-                while ((line = br.readLine()) != null) {
-                    System.out.println("클라이언트로 부터 받은 메세지: " + line);
-                    out.println(line);
-                }
+            try (
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter pw =
+                            new PrintWriter(clientSocket.getOutputStream(), true))
+            {
+                Supplier<String> socketIn = () -> {
+                    try {
+                        return br.readLine();
+                    } catch (IOException ex) {
+                        return null;
+                    }
+                };
+                Stream s = Stream.generate(socketIn);
+                s.map(text -> {
+                    System.out.println("클라이언트로 부터 받은 메세지 : " + text);  // while문에서 line의 역할을 함
+                    pw.println(text);
+                    return text;
+                }).allMatch(t -> t != null);
             } catch (IOException e) {
                 throw new RuntimeException(e);
-            } //finally {
-            //  br.close();
-            //}
-        } catch (IOException ex) {
-            // Handle exceptions
+            }
+        }
+        catch (IOException ex) {
+            System.out.println("접속 실패!");
         }
     }
 }

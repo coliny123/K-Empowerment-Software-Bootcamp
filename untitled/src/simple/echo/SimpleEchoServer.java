e4dbe4a6622c12a8ca8726305a98942e2a5abf9f
dohwan <dh990921@naver.com>
2023-02-14 14:19:13 +0900

    TreadServer Pool V0.2

--- untitled/src/simple/echo/SimpleEchoServer.java
+++ untitled/src/simple/echo/SimpleEchoServer.java
@@ -1,48 +1,55 @@
 package simple.echo;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
 import java.net.ServerSocket;
-import java.io.IOException;
-
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+// IP 165.246.115.165 포트 20000
 
 public class SimpleEchoServer implements Runnable {
+    // 다중 접속 에코 서버
     private static Socket clientSocket;
-
-    public SimpleEchoServer(Socket clientSocket) {
+    public SimpleEchoServer(Socket clientSocket)
+    {
         this.clientSocket = clientSocket;
     }
-
-
     public static void main(String[] args) {
-        System.out.println("Threaded Echo Server");
-        try (ServerSocket serverSocket = new ServerSocket(6000)) {
+        ExecutorService eService = Executors.newFixedThreadPool(5);  // 5 threads
+        System.out.println("다중 접속 에코 서버");
+
+        try (ServerSocket serverSocket = new ServerSocket(20000)) {
             while (true) {
-                System.out.println("Waiting for connection.....");
+                System.out.println("클라이언트 접속 대기 중.....");
                 clientSocket = serverSocket.accept();
                 SimpleEchoServer tes = new SimpleEchoServer(clientSocket);
-                new Thread(tes).start();  // Runable 인터페이스에서 implements 함
+                eService.submit(tes);
             }
         } catch (IOException ex) {
-            // Handle exceptions
+            System.out.println("입출력 예외 발생!");
         }
-        System.out.println("Threaded Echo Server Terminating");
+        System.out.println("다중 접속 에코 서버 종료");
+        eService.shutdown();
     }
-        @Override
-        public void run () {
-            System.out.println("Connected to client using [" + Thread.currentThread() + "]");
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-                String inputLine;
-                while ((inputLine = br.readLine()) != null) {
-                    System.out.println("Client request [" + Thread.currentThread() + "]: " + inputLine);
-                    out.println(inputLine);
-                }
-                System.out.println("Client [" + Thread.currentThread() + " connection terminated");
-            } catch (IOException ex) {
-                // Handle exceptions
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread() + " 스레드 접속");
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                System.out.println(Thread.currentThread() +" 클라이언트가 보낸 메세지 : " + inputLine);
+                out.println(inputLine);
             }
+            System.out.println(Thread.currentThread() +" 클라이언트가 종료됨"); }
+        catch (IOException ex)
+        {
+            System.out.println("입출력 예외 발생!");
         }
+    }
 }

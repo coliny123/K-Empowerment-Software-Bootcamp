5559151cb4a631c37b37cf38d79a6085068a08f6
dohwan <dh990921@naver.com>
2023-02-14 11:16:50 +0900

    simpleechosever v03

--- untitled/src/simple/echo/SimpleEchoServer.java
+++ untitled/src/simple/echo/SimpleEchoServer.java
@@ -6,15 +6,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Objects;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -32,19 +24,16 @@ public class SimpleEchoServer {
                     PrintWriter pw =
                             new PrintWriter(clientSocket.getOutputStream(), true))
             {
-                Supplier<String> socketIn = () -> {
+                Stream.generate(()-> {
                     try {
                         return br.readLine();
                     } catch (IOException ex) {
                         return null;
                     }
-                };
-                Stream s = Stream.generate(socketIn);
-                s.map(text -> {
+                }).peek(text -> {  // IED가 map은 쓸모없는 return한다고 생각해서 peek으로 변경해줌
                     System.out.println("클라이언트로 부터 받은 메세지 : " + text);  // while문에서 line의 역할을 함
                     pw.println(text);
-                    return text;
-                }).allMatch(t -> t != null);
+                }).allMatch(Objects::nonNull);  // :: 참조할 때 쓰는 기호
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }

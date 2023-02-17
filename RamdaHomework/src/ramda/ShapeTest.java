4a1307c45e482e705ed30019c07baf844f723c88
dohwan <dh990921@naver.com>
2023-02-17 18:36:53 +0900

    ramda_homework_02

--- RamdaHomework/src/ramda/ShapeTest.java
+++ RamdaHomework/src/ramda/ShapeTest.java
@@ -5,27 +5,32 @@ import java.util.List;
 
 public class ShapeTest {
     public static void main(String[] args) {
-        List<Shape> rectagles = findShapes(Shape.shapes, "사각형" , null, null);
+        List<Shape> rectagles = findShapeByType(Shape.shapes, "사각형");
         System.out.print("사각형 :");
         System.out.println(rectagles);
-        List<Shape> redNSmallShapes = findShapes(Shape.shapes, null, "빨간색", 12.0);
+        List<Shape> redNSmallShapes = findShapesByColorNArea(Shape.shapes, "빨간색", 12.0);
         System.out.print("빨간 도형(면적<=12.0): ");
         System.out.println(redNSmallShapes);
     }
-    static List<Shape> findShapes(List<Shape> shapes, String type, String color, Double area) {
+    static List<Shape> findShapeByType(List<Shape> shapes, String type) {
         List<Shape> result = new ArrayList<>();
 
         for(Shape s : shapes){
-            if(type != null) {
-                if (s.getType().equals(type)) {
-                    result.add(s);
-                }
-            }else {
-                if(s.getColor().equals(color) && s.getArea() <= area){
-                    result.add(s);
-                }
+            if(s.getType().equals(type)){
+                result.add(s);
             }
         }
         return result;
     }
+
+    static List<Shape> findShapesByColorNArea(List<Shape> shapes, String color, double area) {
+        List<Shape> result = new ArrayList<>();
+        for (Shape s:shapes){
+            if(s.getColor().equals(color) && s.getArea() <= area){
+                result.add(s);
+            }
+        }
+        return result;
+    }
+
 }

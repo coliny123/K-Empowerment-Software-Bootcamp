33a4af69685240c03cfbcd2ddceee5981387099d
dohwan <dh990921@naver.com>
2023-02-20 13:44:18 +0900

    ThreadHomework_03

--- RamdaHomework/src/ThreadHomework.java
+++ RamdaHomework/src/ThreadHomework.java
@@ -1,19 +1,25 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class ThreadHomework {
     public static void main(String[] args) {
         int alphabet = 'a';
-
-        Thread worker = new Thread(()->{
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Runnable task = ()->{
             try {
-                for(int i = 0; i < 5; i++){
+                for (int i = 0; i < 5; i++){
                     System.out.println("작업 쓰레드 : " + i);
                     Thread.sleep(1000);
                 }
             } catch (InterruptedException e) {
             }
-        });
-        worker.start();
+            exec.shutdown();
+        };
+        exec.execute(task);
+
         try {
-            while (worker.isAlive()){
+            while (!exec.isShutdown()){
                 System.out.println("메인 쓰레드 : " + (char)alphabet++);
                 Thread.sleep(500);
             }
